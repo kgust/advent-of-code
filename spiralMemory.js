6ab72d8d525d9ec98998629ad9081f0eb61496d1
Kevin Gustavson <kdgustavson@gmail.com>
2017-12-25 13:25:47 -0600

    Day 3: Stress test

--- spiralMemory.js
+++ spiralMemory.js
@@ -3,19 +3,18 @@
 function calculate(input) {
     input = parseInt(input, 10);
     let [x,y]=[0,0];
-    let [inc,dir,mem]=[1,1,1];
-
-    if (input === 1) return 0;
-
+    let [inc,dir]=[1,1];
+    let memory = {"0,0":1};
+    let g = function(x,y) { return memory[x+","+y] || 0};
     for (;;) {
-        for (let i = 1; i < inc + 1; i++) {
-            mem++;
-            x = (dir === 1) ? x + 1 : (dir === 3) ? x - 1 : x;
-            y = (dir === 2) ? y + 1 : (dir === 4) ? y - 1 : y;
-            if (mem === input) return Math.abs(x) + Math.abs(y);
+        for (let i=1;i<inc+1;i++) {
+            x = (dir===1) ? x+1 : (dir===3) ? x-1 : x;
+            y = (dir===2) ? y+1 : (dir===4) ? y-1 : y;
+            memory[x+","+y]=g(x+1,y-1)+g(x+1,y)+g(x+1,y+1)+g(x-1,y-1)+g(x-1,y)+g(x-1,y+1)+g(x,y-1)+g(x,y+1);
+            if (memory[x+","+y]>input) return memory[x+","+y];
         }
-        dir = (dir === 4) ? 1 : dir + 1;
-        if ((dir === 1) || (dir === 3)) inc++;
+        dir = (dir===4) ? 1 : dir+1;
+        if ((dir===1) || (dir===3)) inc++;
     }
 }
 
--- test/spiralMemory.test.js
+++ test/spiralMemory.test.js
@@ -1,14 +1,21 @@
 // spiralMemory.test.js
 
+// Square 1 starts with the value 1.
+// Square 2 has only one adjacent filled square (with value 1), so it also stores 1.
+// Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.
+// Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.
+// Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.
+
+
 const calculate = require('../spiralMemory');
 
 test('expect input to equal output', () => {
     const provider = [
-        [1,0],
-        [12, 3],
-        [23, 2],
-        [1024, 31],
-        [312051, 430]
+        [1,1],
+        [2, 1],
+        [3, 2],
+        [4, 4],
+        [5, 5]
 
     ];
 
