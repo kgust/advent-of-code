f16f75695aadc255bffeeb066c3b035c6731c7e2
Kevin Gustavson <kdgustavson@gmail.com>
2017-12-25 14:53:32 -0600

    Day 4: No Anagrams

--- passphraseValidator.js
+++ passphraseValidator.js
@@ -3,6 +3,10 @@ const _ = require('lodash');
 
 function validator(input) {
     let values = input.split(' ');
+    values.forEach( (value, index, arr) => {
+        arr[index] = value.split('').sort().join('');
+    });
+
     let uniq = _.uniq(values);
 
     return values.length === uniq.length;
--- test/passphrases.test.js
+++ test/passphrases.test.js
@@ -6,7 +6,8 @@ test('a valid passphrase should contain no duplicate words', () => {
     const provider = [
         ['aa bb cc dd ee', true],
         ['aa bb cc dd aa', false],
-        ['aa bb cc dd aaa', true]
+        ['aa bb cc dd aaa', true],
+        ['gark karg bark', false]
     ];
 
     provider.forEach(values => {
