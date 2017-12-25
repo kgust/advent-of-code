230003c002c00e606f834cfdbf7d641aafc41b7d
Kevin Gustavson <kdgustavson@gmail.com>
2017-12-25 09:55:10 -0600

    Day 2: Evenly divisible

--- spreadsheetChecksum.js
+++ spreadsheetChecksum.js
@@ -7,9 +7,17 @@ function checksum(input) {
     lines.forEach(line => {
         if (line.length < 3) return true;
         let values = line.split("\t").map(Number);
-        let max = Math.max.apply(Math, values);
-        let min = Math.min.apply(Math, values);
-        sum += max - min;
+
+        // TODO find the values that multiples
+        // e.g. 8 / 2 = 4
+        for (let i=0; i<values.length; i++) {
+            for (let j=0; j<values.length; j++) {
+                if (values[i] === values[j]) continue;
+                if (values[i] % values[j] === 0) {
+                    sum += (values[i]/values[j]);
+                }
+            }
+        }
     });
 
     return sum;
--- test/spreadsheet.test.js
+++ test/spreadsheet.test.js
@@ -1,12 +1,24 @@
 // spreadsheet.test.js
 const checksum = require('../spreadsheetChecksum');
 
-test('expect input to equal 18', () => {
+test('expect input to equal 4', () => {
+    expect(checksum([5,9,2,8].join("\t"))).toBe(4);
+});
+
+test('expect input to equal 4', () => {
+    expect(checksum([9,4,7,3].join("\t"))).toBe(3);
+});
+
+test('expect input to equal 4', () => {
+    expect(checksum([3,8,6,5].join("\t"))).toBe(2);
+});
+
+test('expect input to equal 9', () => {
     expect(checksum(
         [
-            [5,1,9,5].join("\t"),
-            [7,5,3].join("\t"),
-            [2,4,6,8].join("\t")
+            [5,9,2,8].join("\t"),
+            [9,4,7,3].join("\t"),
+            [3,8,6,5].join("\t")
         ].join("\n")
-    )).toBe(18);
+    )).toBe(9);
 });
