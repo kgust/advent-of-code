4d6ea79471dc50d8cc839dd3f76d4df298e139c3
Kevin Gustavson <kdgustavson@gmail.com>
2018-01-03 09:17:50 -0600

    Day 5: Part Two

--- jumplist.js
+++ jumplist.js
@@ -5,10 +5,15 @@ let counter = 0;
 function* jumplist(input) {
     while(true) {
         counter++;
-        let jump = input[pointer];
-        // console.log(jump, pointer);
-        input[pointer]++;
-        pointer += jump;
+        let offset = input[pointer];
+
+        if (offset >= 3) {
+            input[pointer]--;
+        } else {
+            input[pointer]++;
+        }
+
+        pointer += offset;
         if (pointer >= input.length) return counter;
         yield pointer;
     }
--- test/jumplist.test.js
+++ test/jumplist.test.js
@@ -7,8 +7,13 @@ test('should escape after 5 jumps', () => {
     expect(generator.next().value).toBe(1);
     expect(generator.next().value).toBe(4);
     expect(generator.next().value).toBe(1);
+    expect(generator.next().value).toBe(3);
+    expect(generator.next().value).toBe(4);
+    expect(generator.next().value).toBe(2);
+    expect(generator.next().value).toBe(2);
+    expect(generator.next().value).toBe(3);
 
     const result = generator.next();
     expect(result.done).toBe(true);
-    expect(result.value).toBe(5);
+    expect(result.value).toBe(10);
 });
