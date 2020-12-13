346bda62c8b91dfc0f1875089464f56b959fe8a4
Kevin Gustavson <kdgustavson@gmail.com>
2020-12-13 12:05:26 -0600

    Day 9: Encoding Error

---
+++ Day/09/EncodingError.php
@@ -0,0 +1,96 @@
+<?php
+declare(strict_types=1);
+
+namespace Day\Nine;
+
+class EncodingError
+{
+    public array $sample = [
+        35, 20, 15, 25, 47,
+        40, 62, 55, 65, 95, 102,
+        117, 150, 182, 127, 219,
+        299, 277, 309, 576
+    ];
+
+    public array $input = [];
+
+    public function __construct()
+    {
+        $file = file_get_contents(__DIR__ . '/input');
+        $this->input = array_map(
+            fn($line) => (int) $line,
+            explode("\n", trim($file))
+        );
+    }
+
+    public function generateSums(array $input, int $start, int $size): array
+    {
+        $end = $start + $size;
+
+        $sums = [];
+        for ($i = $start; $i < $end - 1; $i++)
+        {
+            for ($j = $i + 1; $j < $end; $j++) {
+                $sums[] = $input[$i] + $input[$j];
+            }
+        }
+
+        return $sums;
+    }
+
+    public function findCorruptValue($input, $windowSize): int
+    {
+        $inputSize = count($input);
+
+        for ($i = $windowSize; $i < $inputSize; $i++) {
+            $sums = $this->generateSums($input, $i - $windowSize, $windowSize);
+
+            if (!in_array($input[$i], $sums)) {
+                return $input[$i];
+            }
+        }
+
+        return -1;
+    }
+
+    public function findEncryptionWeakness($input, $corruptValue): int
+    {
+        $inputSize = count($input);
+
+        for ($i = 0; $i < $inputSize; $i++) {
+            file_put_contents('/dev/stderr', $i . "\n", FILE_APPEND);
+            $windowSize = 1;
+            $accumulator = $input[$i];
+            $values = [$input[$i]];
+            while (true) {
+                if ($i + $windowSize > $inputSize) {
+                    break;
+                }
+
+                $accumulator += $input[$i + $windowSize];
+                $values[] = $input[$i + $windowSize];
+
+                if ($accumulator === $corruptValue) {
+                    $value = min($values) + max($values);
+                    $equation = sprintf("%s,%s: %s + %s = %s\n", $i, $windowSize, min($values), max($values), $value);
+
+                    file_put_contents(
+                        '/dev/stderr',
+                        $equation,
+                        FILE_APPEND
+                    );
+
+                    return $value;
+                }
+
+                if ($accumulator > $corruptValue) {
+                    break;
+                }
+
+                $windowSize++;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ Day/09/EncodingErrorTest.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+namespace Day\Nine;
+
+use PHPUnit\Framework\TestCase;
+
+class EncodingErrorTest extends TestCase
+{
+    public function testInput()
+    {
+        $service = new EncodingError();
+        $this->assertSame(13, $service->input[2]);
+    }
+
+    public function testSlidingSums()
+    {
+        $service = new EncodingError();
+        $sums = $service->generateSums($service->sample, 0, 2);
+        $this->assertEquals([55], $sums);
+
+        $sums = $service->generateSums($service->sample, 0, 3);
+        $this->assertEquals([55, 50, 35], $sums);
+
+        $sums = $service->generateSums($service->sample, 0, 4);
+        $this->assertEquals([55, 50, 60, 35, 45, 40], $sums);
+
+        $sums = $service->generateSums($service->sample, 0, 5);
+        $this->assertEquals([55, 50, 60, 82, 35, 45, 67, 40, 62, 72], $sums);
+
+        $sums = $service->generateSums($service->sample, 2, 2);
+        $this->assertEquals([40], $sums);
+    }
+
+    public function testFindCorruptValueInSample()
+    {
+        $service = new EncodingError();
+        $corruptValue = $service->findCorruptValue($service->sample, 5);
+
+        $this->assertEquals(127, $corruptValue);
+    }
+
+    public function testFindCorruptValueInInput()
+    {
+        $service = new EncodingError();
+        $corruptValue = $service->findCorruptValue($service->input, 25);
+
+        $this->assertEquals(393911906, $corruptValue);
+    }
+
+    public function testFindEncryptionWeaknessInSample()
+    {
+        $service = new EncodingError();
+        $weakness = $service->findEncryptionWeakness($service->sample, 127);
+
+        $this->assertEquals(62, $weakness);
+    }
+
+    public function testFindEncryptionWeaknessInInput()
+    {
+        $service = new EncodingError();
+        $weakness = $service->findEncryptionWeakness($service->input, 393911906);
+
+        $this->assertEquals(59341885, $weakness);
+    }
+}
