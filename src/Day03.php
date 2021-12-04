1104a8ca244d3aacb1f032e87350cd347e77233a
Kevin Gustavson <kdgustavson@gmail.com>
2021-12-03 19:58:18 -0600

    Day 3: Binary Diagnostic (Solved!)

---
+++ src/Day03.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Kgust\AdventOfCode;
+
+class Day03
+{
+
+    public static function parseInput(string $path): array
+    {
+        $contents = file_get_contents(__DIR__ . '/../' . $path);
+        $lines = explode("\n", $contents);
+        $lines = array_filter($lines);
+
+        return $lines;
+    }
+
+    public static function calculateGamma(array $lines): array
+    {
+        $gamma = [];
+
+        foreach (range(0, strlen($lines[0]) - 1) as $index) {
+            $ones = 0;
+            $zeros = 0;
+            foreach ($lines as $line) {
+                $line[$index] === '1' ? $ones++ : $zeros++;
+            }
+
+            $gamma[$index] = 0;
+            if ($ones >= $zeros) {
+                $gamma[$index] = 1;
+            }
+        }
+
+        return $gamma;
+    }
+
+    public static function determineO2GeneratorRating(array $numbers, int $position = 0): string
+    {
+        $mostCommon = self::calculateGamma(array_values($numbers))[$position];
+
+        $filtered = array_filter(
+            $numbers,
+            fn(string $number) => (int) $number[$position] === $mostCommon
+        );
+
+        if (count($filtered) === 1) {
+            return array_shift($filtered);
+        }
+
+        return self::determineO2GeneratorRating($filtered, $position + 1);
+    }
+
+    public static function determineCO2ScrubberRating(array $numbers, int $position = 0): string
+    {
+        $mostCommon = self::calculateGamma(array_values($numbers))[$position];
+        $leastCommon = $mostCommon === 1 ? 0 : 1;
+
+        $filtered = array_filter(
+            $numbers,
+            fn(string $number) => (int) $number[$position] === $leastCommon
+        );
+
+        if (count($filtered) === 1) {
+            return array_shift($filtered);
+        }
+
+        return self::determineCO2ScrubberRating($filtered, $position + 1);
+    }
+}
---
+++ tests/Day03Test.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Test\Kgust\AdventOfCode;
+
+use Kgust\AdventOfCode\Day03;
+use PHPUnit\Framework\TestCase;
+
+class Day03Test extends TestCase
+{
+    public function testParseInput(): void
+    {
+        $input = Day03::parseInput('input/Sample03');
+        $this->assertEquals(
+            array_shift($input),
+            '00100'
+        );
+    }
+
+    /**
+     * @dataProvider partOne
+     */
+    public function testGetGamma(string $path, array $expected): void
+    {
+        $lines = Day03::parseInput($path);
+        $gamma = Day03::calculateGamma($lines);
+        $this->assertEquals($expected, $gamma);
+        // 0x18A => 394 * 0xe75 => 3701
+    }
+
+    /**
+     * @dataProvider partTwo
+     */
+    public function testO2GeneratorRating(string $path, array $expected): void
+    {
+        $input = Day03::parseInput($path);
+        $this->assertEquals($expected[0], Day03::determineO2GeneratorRating($input)); // 789
+        $this->assertEquals($expected[1], Day03::determineCO2ScrubberRating($input)); // 3586
+    }
+
+    public static function partOne(): iterable
+    {
+        yield 'sample' => ['input/Sample03', [1,0,1,1,0]];
+        yield 'input' => ['input/Day03', [ 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0 ]];
+    }
+
+    public static function partTwo()
+    {
+        yield 'sample' => ['input/Sample03', ['10111', '01010']];
+        yield 'input' => ['input/Day03', ['001100010101', '111000000010']];
+    }
+}
